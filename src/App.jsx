d101e1a30b4dca36915df5d3c530526835d5b5a5
Deoksu Jeong <jeongdeoksu@gmail.com>
2025-04-21 21:35:08 +0900

    Adding basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react';
 import './App.css';
+import Header from './Compoments/Header';
+import Editor from './Compoments/Editor';
+import List from './Compoments/List';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>TodoList</>;
+  return (
+    <div className="App">
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  );
 }
 
 export default App;

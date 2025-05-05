8bbedd9fc1d5b5adc4a89795810e50a1b2985433
Deoksu Jeong <jeongdeoksu@gmail.com>
2025-05-05 22:14:37 +0900

    Adding List component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
-import Header from './Compoments/Header';
-import Editor from './Compoments/Editor';
-import List from './Compoments/List';
+import Header from './Components/Header';
+import Editor from './Components/Editor';
+import List from './Components/List';
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'React 공부하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '빨래하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '노래 연습하기',
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0);
 
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    }
+    setTodos([newTodo, ...todos ]);
+  }
   return (
     <div className="App">
       <Header />
-      <Editor />
-      <List />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} />
     </div>
   );
 }
